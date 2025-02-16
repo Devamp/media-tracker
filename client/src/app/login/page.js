86a3fc530d3646182e6cece57e22e5f9156b925e
Devam Patel <devamp@pdx.edu>
2025-02-15 18:57:53 -0800

    completed UI for login and sign up

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -1,7 +1,141 @@
-export default function Login() {
+"use client";
+
+import { Mail, Lock, Loader2 } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+// LoginBody component receives wasLoginSuccessful and setWasLoginSuccessful as props
+const LoginBody = ({ setWasLoginSuccessful }) => {
+  const [isLoading, setIsLoading] = useState(false); // state to hold login processing
+  const router = useRouter();
+
+  // handle the logic upon login click
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    const email = event.target["user-email"].value;
+    const password = event.target["user-password"].value;
+
+    const loginData = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      const res = await fetch("http://localhost:5001/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData),
+      });
+
+      setTimeout(() => {
+        if (res.ok) {
+          router.push("/main-menu");
+        } else {
+          setWasLoginSuccessful(false);
+          console.log("Something went wrong.");
+        }
+        setIsLoading(false);
+      }, 1500);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="flex justify-center items-center h-max">
-      <h1 className="text-white text-lg">Welcome to Login!</h1>
+    <form
+      className="w-full max-w-md flex flex-col items-center"
+      onSubmit={handleLogin}
+      method="POST"
+    >
+      <div className="flex flex-row items-center justify-center w-full p-2 rounded-md">
+        <Mail className="mr-2 text-black" size={28} />
+        <input
+          className="p-2 flex-1 rounded-md hover:border-black focus:outline-none"
+          placeholder="Your Email"
+          name="user-email"
+          type="email"
+          required
+        />
+      </div>
+
+      <div className="flex flex-row items-center w-full p-2 rounded-md mb-4">
+        <Lock className="mr-2 text-black" size={28} />
+        <input
+          className="p-2 flex-1 rounded-md hover:border-black focus:outline-none"
+          placeholder="Password"
+          name="user-password"
+          type="password"
+          required
+        />
+      </div>
+
+      <button
+        type="submit"
+        className={`bg-blue-400 w-1/2 p-2 rounded-md text-white transition-colors ${
+          isLoading ? "bg-gray-400 cursor-not-allowed" : "hover:bg-blue-500"
+        }`}
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          <div className="flex flex-row justify-center">
+            <Loader2 className="animate-spin" size={24} />
+            <p className="ml-2 ">Logging in...</p>
+          </div>
+        ) : (
+          "Log in"
+        )}
+      </button>
+    </form>
+  );
+};
+
+const LogoHeader = () => {
+  return (
+    <div className="flex flex-row items-center justify-center border-white border-b py-2">
+      <Image
+        src="/TrackifyLogo.PNG"
+        width={50}
+        height={50}
+        alt="Trackify logo"
+        className="mr-2"
+      />
+      <h2 className="text-5xl font-bold">Trackify</h2>
+    </div>
+  );
+};
+
+export default function Login() {
+  const [wasLoginSuccessful, setWasLoginSuccessful] = useState(true);
+
+  return (
+    <div className="m-auto flex flex-col justify-center items-center mt-28 bg-stone-300 py-10 w-2/5 border rounded-lg">
+      <LogoHeader />
+      <div className="flex justify-center p-3">
+        <h2 className="text-xl font-bold">Log in</h2>
+      </div>
+
+      <LoginBody setWasLoginSuccessful={setWasLoginSuccessful} />
+
+      {!wasLoginSuccessful && (
+        <p className="text-red-600 mt-3">
+          Incorrect email or password. Please try again.
+        </p>
+      )}
+
+      <hr className="w-3/5 mt-10 mb-5 border-t-2 border-white" />
+      <h3 className="font-bold">
+        Don't have an account?{" "}
+        <span className="font-bold hover:underline">
+          <Link href="/signup">Sign Up</Link>
+        </span>
+      </h3>
     </div>
   );
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,43 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 const port = 5001;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/login", (req, res) => {
+  if (req.body) {
+    // logic for verifcation successful
+    const { email, password } = req.body;
+    res.status(200);
+
+  } else {
+    // logic if verification failed
+    res.status(400);
+  }
+
+  // send the server response
+  res.send();
+});
+
+app.post("/signup", (req, res) => {
+  if (req.body) {
+    const { user, email, password } = req.body;
+    console.log(user, email, password);
+    res.status(200);
+  } else {
+    res.status(400);
+  }
+  res.send();
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
