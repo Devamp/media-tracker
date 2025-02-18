12e4b9448dcb654124302c7d1fb81359b2201cbc
Andy <andyduong03@gmail.com>
2025-02-18 14:57:07 -0800

    initial commit

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-max">
-      <h1 className="text-white text-lg">Hello Trackify!</h1>
+      <h1 className="text-white text-9xl">Hello Trackify🎵!</h1>
     </div>
   );
 }
